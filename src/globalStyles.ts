8385a61f591b8e13ef4fce82cd7e3b13ffe6d89a
Marvin Chandra <marvinchan05@gmail.com>
2023-08-10 18:37:29 +0700

    Remove unused Screen styled components

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -10,34 +10,30 @@ export const GlobalStyles = createGlobalStyle`
 `;
 
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-color: ${({ theme }) => theme.bgColor};
-  display: grid;
-  grid-template-rows: 1fr min-content;
-  justify-items: center;
-  align-items: center;
-`;
-
-export const Screen = styled.div`
-  width: 100%;
+    width: 100vw;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.bgColor};
+    display: grid;
+    grid-template-rows: 1fr min-content;
+    justify-items: center;
+    align-items: center;
 `;
 
 export const defaultTheme = {
-  bgColor: "#121213",
-  textColor: {
-    primary: "#FFFFFF",
-  },
-  color: {
-    primary: "#3A3A3C",
-    lighterPrimary: "#1A1A1B",
-    secondary: "#818384",
-  },
-  wordle: {
-    color: {
-      yellow: "#B59F3A",
-      green: "#538D4E",
-      gray: "#3A3A3C",
+    bgColor: "#121213",
+    textColor: {
+        primary: "#FFFFFF",
     },
-  },
+    color: {
+        primary: "#3A3A3C",
+        lighterPrimary: "#1A1A1B",
+        secondary: "#818384",
+    },
+    wordle: {
+        color: {
+            yellow: "#B59F3A",
+            green: "#538D4E",
+            gray: "#3A3A3C",
+        },
+    },
 };
